81b0ac00a654421cc0918b9afeea6bc1b4d29376
courtneycrismore <135356100+courtneycrismore@users.noreply.github.com>
2024-06-26 11:33:33 -0400

    Commit security

--- main.tf
+++ main.tf
@@ -32,10 +32,10 @@ resource "aws_security_group" "blog" {
     name        = "blog"
     description = "Allow http and https in.  Allow everything out."
 
-    vcp_id = data.aws_vcp.default.id
+    vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "blog_http_in"{
+resource "aws_security_group_rule" "blog_http_in" {
     type        = "ingress"
     from_port   = 80
     to_port     = 80
@@ -44,7 +44,7 @@ resource "aws_security_group_rule" "blog_http_in"{
 
     aws_security_group_id = aws_security_group.blog.id
 }
-resource "aws_security_group_rule" "blog_https_in"{
+resource "aws_security_group_rule" "blog_https_in" {
     type        = "ingress"
     from_port   = 443
     to_port     = 443
@@ -53,7 +53,7 @@ resource "aws_security_group_rule" "blog_https_in"{
 
     aws_security_group_id = aws_security_group.blog.id
 }
-resource "aws_security_group_rule" "blog_everything_out"{
+resource "aws_security_group_rule" "blog_everything_out" {
     type        = "egress"
     from_port   = 0
     to_port     = 0
