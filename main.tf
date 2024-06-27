a1f7bd3f05255b168bcf52b22a98b506e8182661
courtneycrismore <135356100+courtneycrismore@users.noreply.github.com>
2024-06-27 15:31:18 -0400

    tag

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "blog"{
   }
 }
 
-module "alb" 
+module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
@@ -56,6 +56,7 @@ module "alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
+}
 
   target_groups = [
     {
